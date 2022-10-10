b6326bb38e84cd6c6c5d3fea56f277d6716e3090
misterrager8 <chemllei0304@gmail.com>
2022-10-10 03:03:48 -0400

    FEATURE: theme toggle

--- SQLeasy/static/main.js
+++ SQLeasy/static/main.js
@@ -1,11 +1,23 @@
-$(document).ready(function() {
-    document.documentElement.setAttribute('data-theme', localStorage.getItem('SQLeasy'));
-});
 
-function changeTheme(theme) {
-    localStorage.setItem('SQLeasy', theme);
+
+
+$(document).ready(function() {{
+    localStorage.getItem('SQLeasy') === 'dark' ? setDark() : setLight();
+}});
+
+function setDark() {{
+    localStorage.setItem('SQLeasy', 'dark');
     document.documentElement.setAttribute('data-theme', localStorage.getItem('SQLeasy'));
-}
+    $('#dark').show();
+    $('#light').hide();
+}}
+
+function setLight() {{
+    localStorage.setItem('SQLeasy', 'light');
+    document.documentElement.setAttribute('data-theme', localStorage.getItem('SQLeasy'));
+    $('#light').show();
+    $('#dark').hide();
+}}
 
 function toggleDiv(divId) {
     $('#' + divId).fadeToggle(150);
